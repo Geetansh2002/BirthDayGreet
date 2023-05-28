7d54d7008d5a0dd6d84a0ea33a9d48a354e1c386
Geetansh2002 <geetanshdwivedi1@gmail.com>
2023-05-28 19:54:59 +0530

    Initial commit

--- app/src/main/java/com/example/birthdaygreet/MainActivity.kt
+++ app/src/main/java/com/example/birthdaygreet/MainActivity.kt
@@ -5,23 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val button =findViewById<Button>(R.id.button1)
 
         val age=findViewById<EditText>(R.id.age).text.toString()
-        val name=findViewById<EditText>(R.id.name).text.toString()
 
+        val name=findViewById<EditText>(R.id.name).editableText.toString()
 
         button.setOnClickListener{
             val intent=Intent(this,greeting::class.java)
             intent.putExtra("name",name)
-            intent.putExtra("agee",age)
+            intent.putExtra("age", age)
             startActivity(intent)
         }
     }
